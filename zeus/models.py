c535ebb44cfcfd409b24e5bd25cc225534932b50
unknown <adadenizseker@gmail.com>
2019-10-03 21:49:43 +0300

    prepreation done

--- zeus/models.py
+++ zeus/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+
+class Post(models.Model):
+    author=models.ForeignKey("auth.User",on_delete=models.CASCADE)
+    image=models.ImageFields(blank=True)
+    caption=models.TextField()
+    created_date=models.DateTimeField(default=timezone.now)
